c6ec5e6ebdc9a4b42abd2c08da5b3b19b96905d4
musicbot01 <95924094+musicbot01@users.noreply.github.com>
2022-01-21 08:10:28 +0530

    Update main2.0.py

--- main2.0.py
+++ main2.0.py
@@ -2,7 +2,7 @@ from telethon import TelegramClient, events, Button
 
 app_id = "9037035"
 api_hash = "8d560b235403170a2c9fae863957dbc3"
-token = "5231613135:AAGrl82YfLfTPJI-wuPyRz9lfb-Vgd_c380"
+token = "5238142620:AAHscpbwTe88KQMxT9gzcf2dg67s4UhxSL0"
 
 client = TelegramClient("Legeeteoy", app_id, api_hash).start(bot_token=token)
 
